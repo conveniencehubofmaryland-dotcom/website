a77fcd63a9f564fc7837c2b30bc12bd3f65dd42a
conveniencehubofmaryland-dotcom <conveniencehubofmaryland@gmail.com>
2026-06-06 12:23:40 -0400

    Create page.tsx
    Add update password page route

---
+++ app/admin/update-password/page.tsx
@@ -0,0 +1,7 @@
+import AdminUpdatePasswordForm from '@/components/AdminUpdatePasswordForm'
+
+export const metadata = { title: 'Update Password - Admin' }
+
+export default function AdminUpdatePasswordPage() {
+  return <AdminUpdatePasswordForm />
+}
