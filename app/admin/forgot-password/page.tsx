8f405cd95cbe9305f1292deedb1ac53fb27032b7
conveniencehubofmaryland-dotcom <conveniencehubofmaryland@gmail.com>
2026-06-06 11:27:33 -0400

    Create page.tsx

---
+++ app/admin/forgot-password/page.tsx
@@ -0,0 +1,7 @@
+import AdminForgotPasswordForm from '@/components/AdminForgotPasswordForm'
+
+export const metadata = { title: 'Forgot Password - Admin' }
+
+export default function AdminForgotPasswordPage() {
+  return <AdminForgotPasswordForm />
+}
