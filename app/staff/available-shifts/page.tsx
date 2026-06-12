10d12daca3cf474666bfc1f9e92967486b61532a
conveniencehubofmaryland-dotcom <conveniencehubofmaryland@gmail.com>
2026-06-12 17:38:56 -0400

    Update page.tsx

--- app/staff/available-shifts/page.tsx
+++ app/staff/available-shifts/page.tsx
@@ -150,7 +150,8 @@ export default function AvailableShifts() {
                   )}
                   {shift.status === 'claimed' && (
                     <p className="text-green-600 font-semibold">Claimed by {shift.staff_name}</p>
-                  )}
+                    )}
+                  </div>
                 </div>
               ))}
             </div>
