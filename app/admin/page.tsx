a463cf124f3dc31e5b982aef027c861a1272401f
conveniencehubofmaryland-dotcom <conveniencehubofmaryland@gmail.com>
2026-07-20 22:52:31 -0400

    Remove metadata export from AdminPage

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,5 @@
 import AdminAuthPage from '@/components/AdminAuthPage'
 
-export const metadata = { title: 'Admin - Sign In' }
-
 export default function AdminPage() {
   return <AdminAuthPage />
 }
