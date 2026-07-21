378f63dc6ceb701dce4684b4ec6f413ed238866a
conveniencehubofmaryland-dotcom <conveniencehubofmaryland@gmail.com>
2026-07-20 20:35:40 -0400

    Remove password reset prompt from AdminLoginForm

--- components/AdminLoginForm.tsx
+++ components/AdminLoginForm.tsx
@@ -81,7 +81,6 @@ export default function AdminLoginForm() {
         >
           {status === 'submitting' ? 'Signing in…' : 'Sign In'}
         </button>
-        <p className="text-center text-xs text-gray-600 mt-4">
           Forgot your password? <a href="/admin/forgot-password" className="text-chm-red hover:underline">Reset it</a>
       </form>
     </div>
