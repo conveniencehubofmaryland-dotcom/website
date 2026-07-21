e38705d01b6c951ac57ba27299c90ccb92a5bf5a
conveniencehubofmaryland-dotcom <conveniencehubofmaryland@gmail.com>
2026-07-20 22:10:06 -0400

    Delete app/admin/login directory

--- app/admin/login/page.tsx
+++
@@ -1,8 +0,0 @@
-
-import AdminLoginForm from '@/components/AdminLoginForm'
-
-export const metadata = { title: 'Admin Login' }
-
-export default function AdminLoginPage() {
-  return <AdminLoginForm />
-}
