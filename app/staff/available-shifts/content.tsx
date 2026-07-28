5dda009d020f5082092debda716d88ad6e1817f3
conveniencehubofmaryland-dotcom <conveniencehubofmaryland@gmail.com>
2026-07-28 17:59:29 -0400

    Add ESLint disable for useEffect dependencies
    Added ESLint disable comment for exhaustive deps warning.

--- app/staff/available-shifts/content.tsx
+++ app/staff/available-shifts/content.tsx
@@ -33,6 +33,7 @@ export default function AvailableShiftsContent() {
 
   useEffect(() => {
     checkAuthorization()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   const checkAuthorization = async () => {
