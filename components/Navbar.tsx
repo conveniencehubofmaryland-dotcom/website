a741d093f09892112d08a32fab06a489eb0a5f89
conveniencehubofmaryland-dotcom <conveniencehubofmaryland@gmail.com>
2026-08-03 20:30:50 -0400

    Reduce logo size in Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -21,7 +21,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-20">
           <Link href="/" className="flex-shrink-0">
-            <img src="/logo.jpeg" alt="CHM Logo" className="h-52 w-auto" />
+            <img src="/logo.jpeg" alt="CHM Logo" className="h-16 w-auto" />
           </Link>
 
           <div className="hidden md:flex items-center gap-6">
